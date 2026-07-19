69d370f373c9aac62212952450ef1c98515b0540
Halison Vitorino <HalisonVitorino@gmail.com>
2026-07-19 11:33:44 -0300

    chore: add cypress-mochawesome-reporter to devDependencies and allow scripts

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  //projectId: "dg89hw",
   viewportWidth: 1440,
   viewportHeight: 900,
   defaultCommandTimeout: 10000,
   video: true,
   screenshotOnRunFailure: true,
+  reporter: "cypress-mochawesome-reporter",
+
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "ServeRest Cypress Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    videoOnFailOnly: true,
+  },
 
   e2e: {
     baseUrl: "https://front.serverest.dev",
+
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+
       return config;
     },
   },
